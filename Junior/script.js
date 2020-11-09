45b964736ce01bdf8e786685f2abcf3542e6519e
KaungMyatPaing <kaungmyatpaing2110@gmail.com>
2020-11-09 17:19:01 +0630

    codewards js junior practice codes

--- Junior/script.js
+++ Junior/script.js
@@ -173,3 +173,127 @@ console.log(descendingOrder(123456789));
 */
 
 // ==========================================================================================================
+
+/**
+ * Persistance Bugger
+ *
+ * Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence,
+ * which is the number of times you must multiply the digits in num until you reach a single digit.
+ *
+ * Examples:
+ *  persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
+                       // and 4 has only one digit
+                       
+    persistence(999) === 4 // because 9*9*9 = 729, 7*2*9 = 126,
+                        // 1*2*6 = 12, and finally 1*2 = 2
+
+    persistence(4) === 0 // because 4 is already a one-digit number
+ */
+
+/*
+function persistence(num) {
+  let numArr = num.toString().split("");
+  let res = 1;
+  let persistenceData = 0;
+
+  if (numArr.length === 1) {
+    return 0;
+  } else {
+    do {
+      for (let i = 0; i < numArr.length; i++) {
+        res *= Number(numArr[i]);
+      }
+
+      numArr = res.toString().split("");
+      res = 1;
+      persistenceData++;
+    } while (numArr.length !== 1);
+  }
+
+  return persistenceData;
+}
+
+console.log(persistence(39));
+console.log(persistence(999));
+console.log(persistence(4));
+*/
+
+// =======================================================================================
+
+/**
+ * Equal Sides of An array
+ *
+ * You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is
+ * equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+ *
+ * For example:
+ * Let's say you are given the array {1,2,3,4,3,2,1}: Your function will return the index 3, because at the 3rd position of the array, the sum of left side of
+ * the index ({1,2,3}) and the sum of the right side of the index ({3,2,1}) both equal 6.
+ *
+ * Let's look at another one.
+ * You are given the array {1,100,50,-51,1,1}: Your function will return the index 1, because at the 1st position of the array, the sum of left side of the
+ * index ({1}) and the sum of the right side of the index ({50,-51,1,1}) both equal 1.
+ *
+ * Last one:
+ * You are given the array {20,10,-80,10,10,15,35}
+ * At index 0 the left side is {}
+ * The right side is {10,-80,10,10,15,35}
+ * They both are equal to 0 when added. (Empty arrays are equal to 0 in this problem)
+ * Index 0 is the place where the left side and right side are equal.
+ *
+ * Note: Please remember that in most programming/scripting languages the index of an array starts at 0.
+ *
+ * Input:
+ * An integer array of length 0 < arr < 1000. The numbers in the array can be any integer positive or negative.
+ *
+ * Output:
+ * The lowest index N where the side to the left of N is equal to the side to the right of N. If you do not find an index that fits these rules, then you will
+ * return -1.
+ *
+ * Note:
+ * If you are given an array with multiple answers, return the lowest correct index.
+ */
+
+/*
+function findEvenIndex(arr) {
+  if (arr.length === 1) {
+    return 0;
+  } else {
+    let left_total = 0;
+    let right_total = 0;
+    var correct_idx = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (i !== 0) {
+        left_total = arr
+          .slice(0, i)
+          .reduce((accumulator, currentValue) => accumulator + currentValue);
+      }
+
+      if (i !== arr.length - 1) {
+        right_total = arr
+          .slice(i + 1, arr.length)
+          .reduce((accumulator, currentValue) => accumulator + currentValue);
+      }
+
+      if (left_total === right_total) {
+        correct_idx.push(i);
+      }
+
+      left_total = 0;
+      right_total = 0;
+    }
+
+    if (correct_idx.length === 0) {
+      return -1;
+    }
+
+    return correct_idx[0];
+  }
+}
+
+console.log(findEvenIndex([1, 2, 3, 4, 3, 2, 1]));
+console.log(findEvenIndex([1, 100, 50, -51, 1, 1]));
+console.log(findEvenIndex([20, 10, -80, 10, 10, 15, 35]));
+*/
+
+// ========================================================================================
